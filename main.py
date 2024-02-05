8842c02ad96e86466b4172bf7a8fa5b700baf7db
Tanishka Kalra <tanishkakalra0910@gmail.com>
2024-02-05 11:28:12 +0530

    All Compilation Dependency Added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import subprocess
 num_of_prog=3
@@ -30,14 +31,20 @@ def list_c_files():
             c_files.append(file)
     return c_files
 def c_compile(file):
-   # subprocess.run(["gcc",file,"-o",file[]".out"],stdin=input_file , stderr=error_file)
-    error=open("error_file","r") 
+    with open("error.txt","w") as error_file:
+        subprocess.run(["gcc",file,"-o",f"{file[0:-2]}.out"], stderr=error_file)
+    err_f=open("error.txt","r")
+    error=err_f.read()
+    err_f.close() 
     if len(error)==0:
         print("C program Compiled Succesfully")
-    #error.close()
-   #subprocess.run([],stdout=outfile)
+        with open("output.txt","w") as output_file:
+            subprocess.run([f"./{file[0:-2]}.out"],stdout=output_file)
+        subprocess.run(["cat","output.txt"])
+        subprocess.run(["rm","output.txt"])
     else :
         print("Error Found in C program")
+    subprocess.run(["rm","error.txt"])
 def cpp_compile(file):
     with open("error.txt","w") as error_file:
         subprocess.run(["g++",file,"-o",f"{file[0:-4]}.out"] , stderr=error_file)
@@ -54,15 +61,19 @@ def cpp_compile(file):
         print("Error Found in C++ program")
     subprocess.run(["rm","error.txt"])
 def py_compile(file):
-   # subprocess.run(["python3,file],stdin=input_file , stderr=error_file,stdout=output_file)
-    error=open("error_file","r") 
+    with open("error.txt","w") as error_file:
+        with open("output.txt","w") as output_file:
+            subprocess.run(["python3",file],stderr=error_file,stdout=output_file)
+    err_f=open("error.txt","r")
+    error=err_f.read()
+    err_f.close() 
     if len(error)==0:
         print("Python program Compiled Succesfully")
-    #error.close()
-  # subprocess.run([],stdout=outfile)
+        subprocess.run(["cat","output.txt"])
+        subprocess.run(["rm","output.txt"])  
     else :
         print("Error Found in Python program")
-
+    subprocess.run(["rm","error.txt"])
 zip_files=zip_files()
 for file in zip_files:
     with open("log.txt","w") as log:
@@ -78,5 +89,10 @@ for file in zip_files:
     cpp_files = list_cpp_files()
     for file in cpp_files:
         cpp_compile(file)
-  
+    py_files=list_py_files()
+    for file in py_files:
+        py_compile(file)
+    c_files=list_c_files()
+    for file in c_files:
+        c_compile(file)
     os.chdir("../")
