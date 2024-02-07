f7706ce52113b1862328b04d2c88623324cada18
Tanishka Kalra <tanishkakalra0910@gmail.com>
2024-02-07 17:47:52 +0530

    Phase 1 Completed

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-
+import json
 import os
 import subprocess
 num_of_prog=3
+result={}
+f=open('detail.json')
+data=json.load(f)
+f.close()
+directory=data['directory']
 def zip_files():
     zip_file=[]
     all_files=os.listdir()
@@ -31,68 +36,108 @@ def list_c_files():
             c_files.append(file)
     return c_files
 def c_compile(file):
+    filename = file[0:-2]
     with open("error.txt","w") as error_file:
-        subprocess.run(["gcc",file,"-o",f"{file[0:-2]}.out"], stderr=error_file)
+        subprocess.run(["gcc",file,"-o",f"{filename}.out"], stderr=error_file)
     err_f=open("error.txt","r")
     error=err_f.read()
-    err_f.close() 
+    err_f.close()
+    ans=0 
     if len(error)==0:
         print("C program Compiled Succesfully")
         with open("output.txt","w") as output_file:
-            subprocess.run([f"./{file[0:-2]}.out"],stdout=output_file)
-        subprocess.run(["cat","output.txt"])
+            with open(directory+data[filename][0],"r") as input_file:
+                subprocess.run([f"./{filename}.out"],stdout=output_file,stdin=input_file)
+        output=open("output.txt","r").read().strip()
+        actual_output=open(directory+data[filename][1],"r").read().strip()
+        if(output==actual_output):
+            print("Output Matched for",file)
+            ans=1
+        else:
+            print("Ouput mismatched for",file)
         subprocess.run(["rm","output.txt"])
     else :
         print("Error Found in C program")
     subprocess.run(["rm","error.txt"])
+    return ans
 def cpp_compile(file):
+    filename=file[0:-4]
     with open("error.txt","w") as error_file:
         subprocess.run(["g++",file,"-o",f"{file[0:-4]}.out"] , stderr=error_file)
     err_f=open("error.txt","r")
     error=err_f.read()
     err_f.close()
+    ans=0
     if len(error)==0: 
         print("C++ program Compiled Succesfully")
         with open("output.txt","w") as output_file:
-            subprocess.run([f"./{file[0:-4]}.out"],stdout=output_file)
-        subprocess.run(["cat","output.txt"])
+            with open(directory+data[filename][0],"r") as input_file:
+                subprocess.run([f"./{file[0:-4]}.out"],stdout=output_file,stdin=input_file)
+        output=open("output.txt","r").read().strip()
+        actual_output=open(directory+data[filename][1],"r").read().strip()
+        if(output==actual_output):
+            print("Output Matched for",file)
+            ans=1
+        else:
+            print("Ouput mismatched for",file)
+            
+
         subprocess.run(["rm","output.txt"])
     else :
         print("Error Found in C++ program")
     subprocess.run(["rm","error.txt"])
+    return ans
+       
 def py_compile(file):
+    filename=file[0:-3]
     with open("error.txt","w") as error_file:
         with open("output.txt","w") as output_file:
-            subprocess.run(["python3",file],stderr=error_file,stdout=output_file)
+            with open(directory+data[filename][0],"r") as input_file:
+                subprocess.run(["python3",file],stderr=error_file,stdout=output_file,stdin=input_file)
     err_f=open("error.txt","r")
     error=err_f.read()
-    err_f.close() 
+    err_f.close()
+    ans=0 
     if len(error)==0:
         print("Python program Compiled Succesfully")
-        subprocess.run(["cat","output.txt"])
+        output=open("output.txt","r").read().strip()
+        actual_output=open(directory+data[filename][1],"r").read().strip()
+        if(output==actual_output):
+            print("Output Matched for",file)
+            ans=1            
+        else:
+            print("Ouput mismatched for",file)
+
         subprocess.run(["rm","output.txt"])  
     else :
         print("Error Found in Python program")
     subprocess.run(["rm","error.txt"])
+    return ans
 zip_files=zip_files()
 for file in zip_files:
     with open("log.txt","w") as log:
         subprocess.run(["unzip",file],stdout=log)
     subprocess.run(["rm","log.txt"])
 for file in zip_files:
-    if len(zip_files) < num_of_prog:
-        print("Incomplete Assignment")
-        break
     folder_name = file[:-4]
+    result[folder_name]=0
     os.chdir(folder_name)
+    all_files=os.listdir()
+    if len(all_files) < num_of_prog:
+        print("Incomplete Assignment")
+        continue
     subprocess.run(["pwd"])
+    count=0
     cpp_files = list_cpp_files()
     for file in cpp_files:
-        cpp_compile(file)
+        count+=cpp_compile(file)
     py_files=list_py_files()
     for file in py_files:
-        py_compile(file)
+        count+=py_compile(file)
     c_files=list_c_files()
     for file in c_files:
-        c_compile(file)
+        count+=c_compile(file)
+    result[folder_name]=count
     os.chdir("../")
+print(result)
+    
